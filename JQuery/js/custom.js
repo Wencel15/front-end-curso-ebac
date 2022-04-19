5861638abcf7ec49d4a12639751513f384d45d1c
Luciano Wencel <l_wencel@hotmail.com>
2022-04-19 14:34:30 -0300

    treinando jquery

--- JQuery/js/custom.js
+++ JQuery/js/custom.js
@@ -1,6 +1,9 @@
 // instancia jquery e evita conflitos
 
-jQuery( function($){
+// jQuery( function($){
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
 
     let titulos = $('h4') // seletor de tag
     
